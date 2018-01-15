77c3fe1eba3aa06b6589a739b8bd59c649937147
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-15 15:08:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,64 @@
-class Song(name, artist, genre)
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+@@artists = []
+@@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
 
-  @@song_count = 0
-
-  def initialize
-    @@song_count += 1
   end
 
   def self.count
-    @@song_count
+    @@count
   end
 
-  def name=(name)
-    @name = name
+  def self.artists
+    @@artists.uniq!
+
   end
 
-  def name
-    @name
+  def self.genres
+    @@genres.uniq!
   end
 
-  def artist=(artist)
-    @artist = artist
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count.include?(genre)
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 
-  def artist
-    @artist
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.include?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
-  def genre=(genre)
-    @genre = genre
-  end
+end
 
-  def genre
-    @genre
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count.include?(artist)
+      artist_count[artist] += 1
+    else
+      artist_count[artist] = 1
+    end
   end
-
 end
